609cadefaf98b06b45d7d4b3dff29899fef9190e
Fiddler46 <adhyaksh99@gmail.com>
2023-09-18 15:47:21 +0530

    Navbar created

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,11 @@
 import './index.scss';
 
-const Sidebar = () => (<>Hello from Sidebar</>)
+const Sidebar = () => (
+    <div className='nav-bar'>
+        {/* <Link className='logo' to = '/'>
+            
+        </Link> place for future logo?    */}
+    </div>
+)
 
 export default Sidebar
