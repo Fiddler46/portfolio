cfd72b8038eb183301437af2cbf73c54a9a1de96
Fiddler46 <adhyaksh99@gmail.com>
2023-09-20 19:48:10 +0530

    Navbar layed out with icons and animations

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom';
 import './index.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -16,9 +17,22 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
             <NavLink exact ='true' activeclassname ='active' className="contact-link" to = '/contact'>
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
             </NavLink>
         </nav>
+
+        <ul>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/adhyaksh-guhan-40090a171/'>
+                    <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e'/>
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://github.com/Fiddler46'>
+                    <FontAwesomeIcon icon={faGithub} color='#4d4d4e'/>
+                </a>
+            </li>
+        </ul>
     
     </div>
     
