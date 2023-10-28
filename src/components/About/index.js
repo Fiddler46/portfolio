6836a0dc79f1df7ce53f8af1f61850248e441ce9
Fiddler46 <adhyaksh99@gmail.com>
2023-10-28 16:34:21 +0530

    added about me text and changed some stylings

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,18 +9,29 @@ const About = () => {
       <Sidebar />
       <div className="container about-page">
         <div class="wave-container2">
-          <img className="waveUpsidedown" src={layeredwaves} alt="waves in about page" />
+          <img
+            className="waveUpsidedown"
+            src={layeredwaves}
+            alt="waves in about page"
+          />
         </div>
         <div className="text-zone">
           <h1>About me</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-            tristique accumsan pharetra. Aliquam dui risus, volutpat vel gravida
-            imperdiet, auctor nec justo. Donec commodo neque neque, a blandit
-            neque pellentesque id. Integer faucibus, turpis nec suscipit
-            finibus, ligula metus accumsan ex, ac faucibus odio lectus at nisi.
-            Sed commodo arcu sit amet commodo imperdiet. Phasellus vitae nibh
-            ipsum.
+            I'm Adhyaksh Guhan, a coder with a heart full of backend passion.
+            I'm on a journey to make the digital world run smoother, one line at
+            a time. I've dipped my toes in everything from Discord bots to
+            ethical hacking. Whether it's Node.js, Go, or Ruby, I'm all about
+            solving puzzles and making cool things happen. But I also like
+            making a richer experience utilising the full stack with a focus on
+            accessibility and modern design. 
+            <br />
+            Outside of work, you can find me
+            worldbuilding with my friends in D&D and other tabletop RPGs, and
+            messing about part-time with VA roles in small projects. I also
+            believe Rust and Wasm might be the future of the web and care about
+            community learning so I dedicate my open-source time/learning to
+            those kinds of projects.
           </p>
         </div>
       </div>
